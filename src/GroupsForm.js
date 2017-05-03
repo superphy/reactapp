6200cb48e8401bd2e077edabbeca789aeecb63b0
Kevin Le <kevin.kent.le@gmail.com>
2017-05-03 12:09:37 -0600

    ADD: requests are working fine. just need to display

--- src/GroupsForm.js
+++ src/GroupsForm.js
@@ -2,26 +2,39 @@ import React, { PureComponent } from 'react';
 import Paper from 'react-md/lib/Papers';
 import Group from './Group';
 // Temporary to display fields in class Group
-import {groups, attributes} from './constants';
+import attributes from './constants';
+// axios is a http client lib
+import axios from 'axios'
 
 export default class GroupsForm extends PureComponent {
   constructor(props) {
     super(props);
+    this.state = {
+      groups: []
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleSubmit(event) {
     alert('A name was submitted: ');
     event.preventDefault();
   }
+  componentDidMount() {
+    axios.get(`http://10.139.14.156:8000/api/v0/get_all_attribute_types`)
+      .then(res => {
+        const groups = res.data.data.children.map(obj => obj.data);
+        this.setState({ groups });
+        console.log(groups)
+      });
+  }
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <div className="paper-container">
           <Paper>
-              <Group groups={groups} attributes={attributes}></Group>
+              <Group groups={this.state.groups} attributes={attributes}></Group>
           </Paper>
           <Paper>
-              <Group groups={groups} attributes={attributes}></Group>
+              <Group groups={this.state.groups} attributes={attributes}></Group>
           </Paper>
         </div>
       </form>
