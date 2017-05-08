dd5c3c1565f634f6f2cdb3cc2d220927249bb396
Kevin Le <kevin.kent.le@gmail.com>
2017-05-07 22:20:16 -0600

    ADD: radio button from react-md with fieldset

--- src/components/AddAttribute.js
+++ src/components/AddAttribute.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import SelectField from 'react-md/lib/SelectFields';
+import Radio from 'react-md/lib/SelectionControls/Radio';
 import Divider from 'react-md/lib/Dividers';
 import axios from 'axios'
 import { API_ROOT } from '../middleware/api';
@@ -15,6 +16,7 @@ class AddAttribute extends Component {
     };
     this.setRelation = this.setRelation.bind(this);
     this.setAttribute = this.setAttribute.bind(this);
+    this._handleInlineChange = this._handleInlineChange.bind(this);
   }
   setRelation(newValue, newActiveIndex, event) {
     console.log(newValue, newActiveIndex, event);
@@ -34,33 +36,60 @@ class AddAttribute extends Component {
     const attribute = newValue;
     this.setState({ attribute })
   }
+  _handleInlineChange(e) {
+   // Basically how the `SelectionControlGroup` works
+   this.setState({ inlineValue: e.target.value });
+ }
   render(){
+    const { inlineValue } = this.state;
     return (
       <section className="md-grid">
-        <SelectField
-          id="relations"
-          label="Relations"
-          placeholder="Select a Relation"
-          position={SelectField.Positions.BELOW}
-          menuItems={this.props.relations}
-          className="md-cell"
-          helpOnFocus
-          helpText="Relations are descriptors about specific attributes."
-          onChange={this.setRelation}
-          required
-        />
-        <SelectField
-          id="attributes"
-          label="Attributes"
-          placeholder="Select an Attribute"
-          position={SelectField.Positions.BELOW}
-          menuItems={this.state.attributes}
-          className="md-cell"
-          helpOnFocus
-          helpText="An attribute is a specific instance of a relation type. For example, O157 is an attribute of relation O-Type."
-          onChange={this.setAttribute}
-          required
-        />
+        <fieldset onChange={this._handleInlineChange}>
+          <legend className="md-subheading-1">Select Attribute:</legend>
+          <SelectField
+            id="relations"
+            label="Relations"
+            placeholder="Select a Relation"
+            position={SelectField.Positions.BELOW}
+            menuItems={this.props.relations}
+            className="md-cell"
+            helpOnFocus
+            helpText="Relations are descriptors about specific attributes."
+            onChange={this.setRelation}
+            required
+          />
+          <SelectField
+            id="attributes"
+            label="Attributes"
+            placeholder="Select an Attribute"
+            position={SelectField.Positions.BELOW}
+            menuItems={this.state.attributes}
+            className="md-cell"
+            helpOnFocus
+            helpText="An attribute is a specific instance of a relation type. For example, O157 is an attribute of relation O-Type."
+            onChange={this.setAttribute}
+            required
+          />
+        </fieldset>
+        <fieldset onChange={this._handleInlineChange}>
+          <legend className="md-subheading-1">Link Attributes:</legend>
+          <Radio
+            id="inlineRadio1"
+            inline
+            name="inlineRadios"
+            value="A"
+            label="AND"
+            checked={inlineValue === 'A'}
+          />
+          <Radio
+            id="inlineRadio2"
+            inline
+            name="inlineRadios"
+            value="B"
+            label="OR"
+            checked={inlineValue === 'B'}
+          />
+        </fieldset>
         <Divider />
       </section>
     )
