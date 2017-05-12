d16a5b8c69366234ee5c8fd94c8be442a5fa5bc8
Kevin Le <kevin.kent.le@gmail.com>
2017-05-12 09:55:43 -0600

    ADD: first working/ugly draft of snackbar using material-ui

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,17 +4,28 @@ import ResultsTable from '../components/ResultsTable'
 // axios is a http client lib
 import axios from 'axios'
 import { API_ROOT } from '../middleware/api'
+// Snackbar
+import Snackbar from 'material-ui/Snackbar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Snackbar
+injectTapEventPlugin();
 
 export default class Home extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       jobId: "",
-      hasResult: false
+      hasResult: false,
+      open: false //for the snackbar
     }
     this.handleChangeSubmit = this.handleChangeSubmit.bind(this);
   }
   handleChangeSubmit(groups, target){
+    this.setState({
+      open: true,
+    });
     axios.post(API_ROOT + 'newgroupcomparison', {
       groups: groups,
       target: target
@@ -27,10 +38,24 @@ export default class Home extends PureComponent {
         this.setState({hasResult})
       });
   }
+  // Snackbar
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
   render() {
     return (
       <div className="md-grid">
         {!this.state.hasResult ? <GroupsForm handleChangeSubmit={this.handleChangeSubmit} /> : <ResultsTable jobId={this.state.jobId} />}
+        <MuiThemeProvider>
+          <Snackbar
+            open={this.state.open}
+            message="Comparison was submitted"
+            autoHideDuration={4000}
+            onRequestClose={this.handleRequestClose}
+          />
+        </MuiThemeProvider>
       </div>
     );
   }
