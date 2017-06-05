a95b88a737c252c0d6fb9babc7604217096bae92
Kevin Le <kevin.kent.le@gmail.com>
2017-06-05 14:14:35 -0600

    FIX: reducers for createStore

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'
+import spfyApp from './reducers'
 import createBrowserHistory from 'history/createBrowserHistory'
 import App from './containers/App';
 import './index.css';
