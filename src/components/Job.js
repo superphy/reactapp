c42e95e961239041d48186eff8560fdd00f3dd50
Kevin Le <kevin.kent.le@gmail.com>
2017-06-07 10:25:57 -0600

    ADD: use Card for styling Job

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,11 +1,17 @@
 import React, { PropTypes } from 'react'
+import Card from 'react-md/lib/Cards/Card';
+import CardTitle from 'react-md/lib/Cards/CardTitle';
+import Avatar from 'react-md/lib/Avatars';
+import FontIcon from 'react-md/lib/FontIcons';
 
 const Job = ({ onClick, hash }) => (
-  <li
-    onClick={onClick}
-  >
-    {hash}
-  </li>
+  <Card style={{ maxWidth: 600 }} className="md-block-centered">
+    <CardTitle
+      avatar={<Avatar icon={<FontIcon iconClassName="fa fa-rocket" />} random />}
+      title="Card Title"
+      subtitle={hash}
+    />
+  </Card>
 )
 
 Job.propTypes = {
