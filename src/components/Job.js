5357e929483219e92103825b0c2532890cd72ac4
Kevin Le <kevin.kent.le@gmail.com>
2017-06-07 11:15:17 -0600

    STOP: react-refetch is working for getting results status

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,22 +1,42 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
+import { connect } from 'react-refetch'
 import Card from 'react-md/lib/Cards/Card';
 import CardTitle from 'react-md/lib/Cards/CardTitle';
 import Avatar from 'react-md/lib/Avatars';
 import FontIcon from 'react-md/lib/FontIcons';
+// requests
+import { API_ROOT } from '../middleware/api'
 
-const Job = ({ onClick, hash }) => (
-  <Card style={{ maxWidth: 600 }} className="md-block-centered">
-    <CardTitle
-      avatar={<Avatar icon={<FontIcon iconClassName="fa fa-rocket" />} random />}
-      title="Card Title"
-      subtitle={hash}
-    />
-  </Card>
-)
+class Job extends Component {
+  render(){
+    // set up for status checking
+    const { results } = this.props
+    var complete = false
+    if (results.fulfilled){
+      if (results.value){
+        complete = true
+      } else {
+        complete = false
+      }
+    }
+    // actual card
+    return (
+      <Card style={{ maxWidth: 600 }} className="md-block-centered">
+        <CardTitle
+          avatar={<Avatar icon={<FontIcon iconClassName="fa fa-rocket" />} random />}
+          title={this.props.hash}
+          subtitle={complete ? 'Complete':'Pending'}
+        />
+      </Card>
+    )
+  }
+}
 
 Job.propTypes = {
   onClick: PropTypes.func.isRequired,
   hash: PropTypes.string.isRequired
 }
 
-export default Job
+export default connect(props => ({
+  results: {url: API_ROOT + `results/${props.hash}`, refreshInterval: 5000 }
+}))(Job)
