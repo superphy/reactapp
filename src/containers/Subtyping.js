ea4611c3b2a1a19d3d6a22e0cc1c1e14b733ffec
Kevin Le <kevin.kent.le@gmail.com>
2017-06-09 15:01:24 -0600

    ADD: first draft of POST in react for spfy is done. need to enable CORS for /upload in backend

--- src/containers/Subtyping.js
+++ src/containers/Subtyping.js
@@ -4,12 +4,15 @@ import FileInput from 'react-md/lib/FileInputs';
 import Checkbox from 'react-md/lib/SelectionControls/Checkbox'
 import TextField from 'react-md/lib/TextFields';
 import Button from 'react-md/lib/Buttons';
+// Snackbar
+import Snackbar from 'material-ui/Snackbar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // redux
 import { connect } from 'react-redux'
 import { addJob } from '../actions'
 // axios
 import axios from 'axios'
-import { API_ROOT } from '../middleware/api'
+import { OLD_API } from '../middleware/api'
 
 class Subtyping extends PureComponent {
   constructor(props) {
@@ -20,7 +23,8 @@ class Subtyping extends PureComponent {
       amr: true,
       serotype: true,
       vf: true,
-      open: false
+      open: false,
+      msg: ''
     }
   }
   _selectFile = (file) => {
@@ -41,57 +45,88 @@ class Subtyping extends PureComponent {
     this.setState({ vf: value })
   }
   _handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault() // disable default HTML form behavior
+    this.setState({
+      open: true,
+      msg: "Genomes were submitted"
+    });
+    axios.post(OLD_API + 'upload', {
+      file: this.state.file,
+      options: {
+        pi: this.state.pi,
+        amr: this.state.amr,
+        serotype: this.state.serotype,
+        vf: this.state.vf
+      }
+    }).then(response => {
+      console.log(response)
+    })
+  };
+  // Snackbar
+  _handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
   };
   render(){
     const { file, pi, amr, serotype, vf } = this.state
     return (
-      <form className="md-text-container md-grid">
-        <div className="md-cell md-cell--12">
-          <FileInput
-            id="inputFile"
-            secondary
-            label="Select File(s)"
-            onChange={this._selectFile}
-            multiple
+      <div>
+        <form className="md-text-container md-grid">
+          <div className="md-cell md-cell--12">
+            <FileInput
+              id="inputFile"
+              secondary
+              label="Select File(s)"
+              onChange={this._selectFile}
+              multiple
+            />
+            <Checkbox
+              id="serotype"
+              name="check serotype"
+              checked={serotype}
+              onChange={this._updateSerotype}
+              label="Serotype"
+            />
+            <Checkbox
+              id="vf"
+              name="check vf"
+              checked={vf}
+              onChange={this._updateVf}
+              label="Virulence Factors"
+            />
+            <Checkbox
+              id="amr"
+              name="check amr"
+              checked={amr}
+              onChange={this._updateAmr}
+              label="Antimicrobial Resistance"
+            />
+            <TextField
+              id="pi"
+              value={pi}
+              onChange={this._updatePi}
+              helpText="Percent Identity for BLAST"
+            />
+            <Button
+              raised
+              secondary
+              type="submit"
+              label="Submit"
+              disabled={!file}
+              onClick={this._handleSubmit}
+            />
+          </div>
+        </form>
+        <MuiThemeProvider>
+          <Snackbar
+            open={this.state.open}
+            message={this.state.msg}
+            autoHideDuration={4000}
+            onRequestClose={this.handleRequestClose}
           />
-          <Checkbox
-            id="serotype"
-            name="check serotype"
-            checked={serotype}
-            onChange={this._updateSerotype}
-            label="Serotype"
-          />
-          <Checkbox
-            id="vf"
-            name="check vf"
-            checked={vf}
-            onChange={this._updateVf}
-            label="Virulence Factors"
-          />
-          <Checkbox
-            id="amr"
-            name="check amr"
-            checked={amr}
-            onChange={this._updateAmr}
-            label="Antimicrobial Resistance"
-          />
-          <TextField
-            id="pi"
-            value={pi}
-            onChange={this._updatePi}
-            helpText="Percent Identity for BLAST"
-          />
-          <Button
-            raised
-            secondary
-            type="submit"
-            label="Submit"
-            disabled={!file}
-            onClick={this._handleSubmit}
-          />
-        </div>
-      </form>
+        </MuiThemeProvider>
+      </div>
     )
   }
 }
--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,5 +1,7 @@
 //export const API_ROOT = window.location.protocol + '//' + window.location.hostname + ':8000/api/v0/'
-export const API_ROOT = 'http://10.139.14.212:8000/api/v0/'
+const ROOT = 'http://10.139.14.212:8000/'
+export const API_ROOT = ROOT + 'api/v0/'
+export const OLD_API = ROOT
 
 export const analyses = [{
   'analysis':'subtyping',
