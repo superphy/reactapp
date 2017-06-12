7cc10c200c8be7a29e778203981109ab9133cf02
Kevin Le <kevin.kent.le@gmail.com>
2017-06-12 12:30:43 -0600

    ADD: table for subtyping is displaying, but cols in wrong order

---
+++ src/components/ResultSubtyping.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { connect } from 'react-refetch'
+import DataTable from 'react-md/lib/DataTables/DataTable';
+import TableHeader from 'react-md/lib/DataTables/TableHeader';
+import TableBody from 'react-md/lib/DataTables/TableBody';
+import TableRow from 'react-md/lib/DataTables/TableRow';
+import TableColumn from 'react-md/lib/DataTables/TableColumn';
+// progress bar
+import CircularProgress from 'react-md/lib/Progress/CircularProgress';
+// requests
+import { API_ROOT } from '../middleware/api'
+
+const headers = [
+  "Filename",
+  "Contig ID",
+  "Analysis",
+  "Hit",
+  "Orientation",
+  "Start",
+  "Stop",
+  "Cutoff"
+]
+
+class ResultSubtyping extends Component {
+  render() {
+    const { results } = this.props
+    if (results.pending){
+      return <div>Waiting for server response...<CircularProgress key="progress" id='contentLoadingProgress' /></div>
+    } else if (results.rejected){
+      return <div>Couldn't retrieve job: {this.props.jobId}</div>
+    } else if (results.fulfilled){
+      console.log(results)
+      const rows = results.value.map((row, i) => (
+        <TableRow key={i}>
+          {console.log(row)}
+          {Object.keys(row).map((value, ci) => (
+            <TableColumn key={ci}>{row[value]}</TableColumn>
+          ))}
+        </TableRow>
+      ));
+      return (
+        <DataTable plain>
+          <TableHeader>
+            <TableRow>
+              {headers.map((value, i) => (
+                <TableColumn key={i}>{value}</TableColumn>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {rows}
+          </TableBody>
+        </DataTable>
+      );
+    }
+  }
+}
+
+export default connect(props => ({
+  results: {url: API_ROOT + `results/${props.jobId}`}
+}))(ResultSubtyping)
