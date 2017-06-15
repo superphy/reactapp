6930026717cba16c200698e908f99a590d55269b
Kevin Le <kevin.kent.le@gmail.com>
2017-06-15 09:32:27 -0600

    UPDATE: api root set to read based on location

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,6 +1,6 @@
 import React from 'react'
-//export const API_ROOT = window.location.protocol + '//' + window.location.hostname + ':8000/api/v0/'
-const ROOT = 'http://10.139.14.212:8000/'
+const ROOT = window.location.protocol + '//' + window.location.hostname + ':8000/'
+//const ROOT = 'http://10.139.14.212:8000/'
 export const API_ROOT = ROOT + 'api/v0/'
 export const OLD_API = ROOT
 
