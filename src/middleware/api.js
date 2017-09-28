9d414bae77be88d6d0bed1b8da6517f490a69b4e
Kevin Le <kevin.kent.le@gmail.com>
2017-09-28 12:11:27 -0400

    FIX: api route

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,7 +1,7 @@
 import React from 'react'
-// const ROOT = window.location.protocol + '//' + window.location.hostname + ':8000/'
+const ROOT = window.location.protocol + '//' + window.location.hostname + ':8000/'
 // const ROOT = 'http://10.139.14.212:8000/'
-const ROOT = 'http://192.168.0.16:8000/'
+// const ROOT = 'http://192.168.0.16:8000/'
 export const API_ROOT = ROOT + 'api/v0/'
 export const OLD_API = ROOT
 
