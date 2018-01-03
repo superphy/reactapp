2c161d229cbebfd44f43f74bd9cccc8391c13863
kevinkle <kevin.kent.le@gmail.com>
2018-01-03 09:50:11 -0600

    FIX: corefacility configs

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 //const ROOT = window.location.protocol + '//' + 'spfy.enchartus.ca' + '/'
-const ROOT = window.location.protocol + '//' + window.location.hostname + ':8000/'
+//const ROOT = window.location.protocol + '//' + window.location.hostname + ':8000/'
+const ROOT = 'https://lfz.corefacility.ca/superphy/spfyapi/'
 // const ROOT = 'http://10.139.14.212:8000/'
 // const ROOT = 'http://192.168.5.19:8000/'
 // const ROOT = 'https://spfy.enchartus.ca/'
@@ -58,11 +59,13 @@ export const createErrorMessage = (jobId, msg='') => {
   return message
 }
 
+export const dirpath = '/superphy/spfy'
+
 // auth0 configs
 
 export const CLIENT_ID = '6TNNpuXZmZaQfnd8m5Jm6y1YS6fqKSmT';
 export const CLIENT_DOMAIN = 'spfy.auth0.com';
-export const REDIRECT = 'http://localhost:8090/callback';
+export const REDIRECT = 'https://lfz.corefacility.ca/superphy/spfy/callback';
 export const SCOPE = 'openid';
 export const AUDIENCE = 'https://lfz.corefacility.ca/superphy/spfyapi/';
 
