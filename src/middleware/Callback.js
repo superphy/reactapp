5def533545ca1460632a4aea6e6d04be6e913c9d
Kevin Le <kevin.kent.le@gmail.com>
2018-01-02 23:35:06 -0500

    ADD: slight hack to get callback to play nicely with fetching jobs

--- src/middleware/Callback.js
+++ src/middleware/Callback.js
@@ -3,13 +3,32 @@ import { LinearProgress } from 'react-md'
 import { Redirect } from 'react-router'
 
 class Callback extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sleeping: true
+    };
+  }
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  async wait() {
+    console.log('Taking a break...');
+    await this.sleep(3000);
+    console.log('3 second later');
+    this.setState({sleeping: false})
+  }
+  componentDidMount(){
+    this.wait()
+  }
   render() {
     console.log('Callback has')
     console.log(this.props)
-    const { isAuthenticated } = this.props.auth;
+    // const { isAuthenticated } = this.props.auth;
+    let { sleeping } = this.state
     return (
       <div>
-      {!isAuthenticated()?
+      {sleeping?
         <div>
             <LinearProgress key="progress" id='contentLoadingProgress' />
             <p>
