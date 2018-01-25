e1aaf14c30261a0cce0e9470708c9e62b6e55b6e
Kevin Le <kevin.kent.le@gmail.com>
2018-01-25 14:28:24 -0500

    FIX: mapped store to results

--- src/containers/Results.js
+++ src/containers/Results.js
@@ -1,14 +1,35 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import JobsList from '../components/JobsList'
+import { withRouter } from 'react-router';
+import Job from '../components/Job'
+import { RedirectToken } from '../components/RedirectToken'
 
-const mapStateToProps = (state) => {
-  return {
-    jobs: state.jobs
+class Results extends Component {
+  render(){
+    const { jobs } = this.props;
+    return (
+      <div>
+        <RedirectToken token={this.props.token} />
+        {jobs.map(job =>
+          <Job
+            key={job.id}
+            {...job}
+          />
+        )}
+      </div>
+    )
   }
 }
 
-const Results = connect(
+const mapStateToProps = (state, ownProps) => {
+  return {
+    jobs: state.jobs,
+    ...ownProps
+  }
+}
+
+Results = withRouter(connect(
   mapStateToProps
-)(JobsList)
+)(Results))
 
 export default Results
